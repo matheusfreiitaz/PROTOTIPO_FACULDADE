be776b9d85dea2b12e8f5752b4aeff5da4751348
Matheus Felipe <matheusfreiitas77@gmail.com>
2025-10-10 09:23:30 -0300

    Lista de compras.

---
+++ to-do-list/script.js
@@ -0,0 +1,18 @@
+const botao = document.getElementById("botao")
+const lista= document.getElementById("lista")
+const tarefa= document.getElementById("novaTarefa")
+
+function adicionar(){
+    const base =tarefa.value;
+    const novoValor =document.createElement("div")
+    novoValor.textContent= base;
+    lista.appendChild(novoValor)
+    tarefa.value="";
+   
+}
+botao.addEventListener('click',adicionar)
+
+
+
+
+
