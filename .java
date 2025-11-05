286c432dac4851011bd1aa18dc02f5f21cd8fbe6
Matheus Felipe <matheusfreiitas77@gmail.com>
2025-11-05 10:38:41 -0300

    Create .java

---
+++ .java
@@ -0,0 +1,16 @@
+package introdução;
+
+public class CaixaEletronica {
+    public static void main (String[]argus){
+        double saldo = 27.0;
+        double valorSolicitado= 10.0;
+
+        if(valorSolicitado < saldo ){
+            saldo = saldo - valorSolicitado;
+        System.out.println("Transferência Realizada !");
+    }else {
+        System.out.println("Sem Saldo !");
+        }
+    }
+
+}
