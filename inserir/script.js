dad3953713f643a8d1aec7481e5293c11586289b
Matheus Felipe <matheusfreiitas77@gmail.com>
2025-12-11 22:30:49 -0300

    Refactor script.js for task addition functionality

--- inserir/script.js
+++ inserir/script.js
@@ -1,7 +1,7 @@
 const tarefas = document.getElementById("campo-tarefa");
 const adicionar = document.getElementById("btn-adicionar");
 const lista = document.getElementById("lista-de-tarefas"); 
- 
+  
 function adicionar1 (){ 
     const adicionarTarefa = tarefas.value;
     const novoValor = document.createElement("li") 
@@ -11,6 +11,7 @@ function adicionar1 (){
 } 
 
 adicionar.addEventListener('click',adicionar1)
+
 
 
 
