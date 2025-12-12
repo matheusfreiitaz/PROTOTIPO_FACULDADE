1851175dba17ccfdf79d9fef56f1ca6f59f085d9
Matheus Felipe <matheusfreiitas77@gmail.com>
2025-12-11 22:30:59 -0300

    Remove unnecessary whitespace in script.js

--- inserir/script.js
+++ inserir/script.js
@@ -2,7 +2,7 @@ const tarefas = document.getElementById("campo-tarefa");
 const adicionar = document.getElementById("btn-adicionar");
 const lista = document.getElementById("lista-de-tarefas"); 
   
-function adicionar1 (){ 
+function adicionar1 (){  
     const adicionarTarefa = tarefas.value;
     const novoValor = document.createElement("li") 
     novoValor.textContent = adicionarTarefa
@@ -11,6 +11,7 @@ function adicionar1 (){
 } 
 
 adicionar.addEventListener('click',adicionar1)
+
 
 
 
